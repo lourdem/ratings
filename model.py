fe75bc82d9fc386361b0ffe1e17a9425c7cc0b7e
Your Name <you@example.com>
2020-02-25 10:01:24 -0800

    Part 2 continued

--- model.py
+++ model.py
@@ -50,8 +50,12 @@ class Rating(db.Model):
     movie_id = db.Column(db.Integer,
                          db.ForeignKey('movies.movie_id'))
     user_id = db.Column(db.Integer,
-                
+                        db.ForeignKey('users.user_id'))
     score = db.Column(db.Integer)
+
+    #Define relationship to user
+    user = db.relationship("User", 
+                           backref=db.backref("ratings", order_by=rating_id))
 
     def __repr__(self):
         return f""""<Rating rating_id ={self.rating_id}
